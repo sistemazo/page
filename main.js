f64246e414523053115e0e36a6ce24ecc475bdfc
sistemazo <sistemas1.capnaparo@locatelve.com>
2022-10-31 09:34:13 -0400

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle('windows-scroll',window.scrollY > 0)
+});
+
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open');
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = 'uil uil-minus';
+        }else{
+            icon.className = 'uil uil-plus';
+        }
+    })
+})
+
